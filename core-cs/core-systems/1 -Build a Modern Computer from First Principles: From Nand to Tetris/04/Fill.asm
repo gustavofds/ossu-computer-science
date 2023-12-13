b725da9a4c4980a80146047789e9a1bfc019b003
Gustavo Sousa <gustavohl98@gmail.com>
2023-12-12 21:10:53 -0300

    nand-2-tetris-I: fix Fill.asm to color all screen pixels and pass automatic test

--- core-cs/core-systems/1 -Build a Modern Computer from First Principles: From Nand to Tetris/04/Fill.asm
+++ core-cs/core-systems/1 -Build a Modern Computer from First Principles: From Nand to Tetris/04/Fill.asm
@@ -6,7 +6,7 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen
 // by writing 'black' in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen by writing
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
@@ -16,7 +16,7 @@
 D=A
 @addr
 M=D
-@8190
+@8191
 D=A
 @end
 M=D
